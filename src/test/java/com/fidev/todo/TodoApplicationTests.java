39835af7b86d618f0dd664417315c354bf7cb3dc
Fide Islas <fidev.id@outlook.com>
2022-03-12 17:39:26 -0600

    first commit

---
+++ src/test/java/com/fidev/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fidev.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
